2c5ee95b90898243e8fb8ced564dbfd4b973fddb
Luka Košćak <koscak.luka@gmail.com>
2024-11-03 22:43:37 +0200

    feat(themes): add poimandres colorscheme

--- lua/tschucky/plugins/colorschemas.lua
+++ lua/tschucky/plugins/colorschemas.lua
@@ -35,6 +35,28 @@ return {
 			end,
 		},
 	},
+	{
+		"olivercederborg/poimandres.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("poimandres").setup({
+				-- leave this setup function empty for default config
+				-- or refer to the configuration section
+				-- for configuration options
+				-- bold_vert_split = false, -- use bold vertical separators
+				-- dim_nc_background = false, -- dim 'non-current' window backgrounds
+				disable_background = true, -- disable background
+				-- disable_float_background = false, -- disable background for floats
+				-- disable_italics = false, -- disable italics
+			})
+		end,
+
+		-- optionally set the colorscheme within lazy config
+		init = function()
+			vim.cmd("colorscheme poimandres")
+		end,
+	},
 	-- {
 	--     "folke/tokyonight.nvim",
 	--     lazy = false,
--- lua/tschucky/plugins/statusline.lua
+++ lua/tschucky/plugins/statusline.lua
@@ -5,7 +5,7 @@ return {
 	opts = {
 		options = {
 			icons_enabled = false,
-			theme = "catppuccin",
+			theme = "poimandres",
 			component_separators = "|",
 			section_separators = "",
 		},
