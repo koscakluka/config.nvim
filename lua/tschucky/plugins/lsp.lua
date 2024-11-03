780a47a5aa7a4dc77aba214aa11c7bde2006dbc6
Luka Košćak <koscak.luka@gmail.com>
2024-11-03 22:49:28 +0200

    feat(lsp): add better-ts-errors

--- lua/tschucky/plugins/lsp.lua
+++ lua/tschucky/plugins/lsp.lua
@@ -261,6 +261,16 @@ return {
 			})
 		end,
 	},
+	{
+		"OlegGulevskyy/better-ts-errors.nvim",
+		dependencies = { "MunifTanjim/nui.nvim" },
+		config = {
+			keymaps = {
+				toggle = "<leader>dt", -- default '<leader>dd'
+				go_to_definition = "<leader>dx", -- default '<leader>dx'
+			},
+		},
+	},
 	-- {
 	-- 	"jmbuhr/otter.nvim",
 	-- 	dependencies = {
