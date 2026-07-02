f3c60fb00b085c46590f75f0e7f61d1a1a25cff9
Luka Košćak <koscak.luka@gmail.com>
2026-07-02 11:56:17 +0200

    feat(statusline): switch theme to rose-pine
    Replace the old colorscheme setup with rose-pine.
    
    Keep lualine aligned with the active theme.

--- lua/tschucky/plugins/colorschemas.lua
+++ lua/tschucky/plugins/colorschemas.lua
@@ -12,51 +12,51 @@ return {
 	-- 		require("onedark").load()
 	-- 	end,
 	-- },
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			flavour = "mocha",
-			transparent_background = true,
-			integrations = {
-				harpoon = true,
-				notify = true,
-				treesitter_context = true,
-			},
-			custom_highlights = function(colors)
-				return {
-					Comment = { fg = colors.subtext1 },
-					LineNr = { fg = colors.text },
-					LineNrAbove = { fg = colors.overlay2 },
-					LineNrBelow = { fg = colors.overlay2 },
-				}
-			end,
-		},
-	},
-	{
-		"olivercederborg/poimandres.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("poimandres").setup({
-				-- leave this setup function empty for default config
-				-- or refer to the configuration section
-				-- for configuration options
-				-- bold_vert_split = false, -- use bold vertical separators
-				-- dim_nc_background = false, -- dim 'non-current' window backgrounds
-				disable_background = true, -- disable background
-				-- disable_float_background = false, -- disable background for floats
-				-- disable_italics = false, -- disable italics
-			})
-		end,
-
-		-- optionally set the colorscheme within lazy config
-		init = function()
-			vim.cmd("colorscheme poimandres")
-		end,
-	},
+	-- {
+	-- 	"catppuccin/nvim",
+	-- 	name = "catppuccin",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {
+	-- 		flavour = "mocha",
+	-- 		transparent_background = true,
+	-- 		integrations = {
+	-- 			harpoon = true,
+	-- 			notify = true,
+	-- 			treesitter_context = true,
+	-- 		},
+	-- 		custom_highlights = function(colors)
+	-- 			return {
+	-- 				Comment = { fg = colors.subtext1 },
+	-- 				LineNr = { fg = colors.text },
+	-- 				LineNrAbove = { fg = colors.overlay2 },
+	-- 				LineNrBelow = { fg = colors.overlay2 },
+	-- 			}
+	-- 		end,
+	-- 	},
+	-- },
+	-- {
+	-- 	"olivercederborg/poimandres.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("poimandres").setup({
+	-- 			-- leave this setup function empty for default config
+	-- 			-- or refer to the configuration section
+	-- 			-- for configuration options
+	-- 			-- bold_vert_split = false, -- use bold vertical separators
+	-- 			-- dim_nc_background = false, -- dim 'non-current' window backgrounds
+	-- 			disable_background = true, -- disable background
+	-- 			-- disable_float_background = false, -- disable background for floats
+	-- 			-- disable_italics = false, -- disable italics
+	-- 		})
+	-- 	end,
+	--
+	-- 	-- optionally set the colorscheme within lazy config
+	-- 	init = function()
+	-- 		vim.cmd("colorscheme poimandres")
+	-- 	end,
+	-- },
 	-- {
 	--     "folke/tokyonight.nvim",
 	--     lazy = false,
@@ -65,4 +65,17 @@ return {
 	--         transparent = true
 	--     },
 	-- },
+	--
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				-- variant = "moon",
+				styles = { italic = false, transparency = true},
+				highlight_groups = { Visual = { bg = "text", inherit = false, blend = 35 } },
+			})
+			vim.cmd("colorscheme rose-pine")
+		end,
+	},
 }
--- lua/tschucky/plugins/statusline.lua
+++ lua/tschucky/plugins/statusline.lua
@@ -5,7 +5,7 @@ return {
 	opts = {
 		options = {
 			icons_enabled = false,
-			theme = "poimandres",
+			theme = "rose-pine",
 			component_separators = "|",
 			section_separators = "",
 		},
